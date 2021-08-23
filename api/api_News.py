053927eb72fcb556202b12496e8b6984d210f4d9
weipeng <137434122@qq.com>
2021-08-23 22:31:56 +0800

    ss

--- api/api_News.py
+++ api/api_News.py
@@ -4,10 +4,13 @@ Create time: 2021/2/14
 Developer: 韦鹏
 '''
 import os
+
+import requests
+
 from common.http_requests import HttpRequests
 
 
-class Api_Auth_Service(object):
+class Api_News_Service(object):
 
     def __init__(self):
         self.headers = HttpRequests().headers
@@ -19,9 +22,20 @@ class Api_Auth_Service(object):
         hot = response.json()['result']['data'][0]['title']
         return hot
 
+    def get_weather(self,city):
+        url='http://apis.juhe.cn/simpleWeather/query'
+        param_data = {'city':city,'key': 'fea71ec5629c802f1c7d64ba57bae351'}
+        response = HttpRequests().get(url,params=param_data,headers=self.headers)
+        return response
+
 
 
 if __name__ == '__main__':
     #Auth_Service().api_home_service_list()
-    print(Api_Auth_Service().get_first_hot())
+    print(Api_News_Service().get_weather('杭州'))
     #Auth_Service().api_service_info()
+    # s=requests.session()
+    # url = 'http://apis.juhe.cn/simpleWeather/query'
+    # param_data = {'city': "杭州", 'key': 'fea71ec5629c802f1c7d64ba57bae351'}
+    # ss = s.get(url,params=param_data)
+    # print(ss)
--- run_main.py
+++ run_main.py
@@ -3,4 +3,4 @@ import pytest
 
 if __name__ == '__main__':
     pytest.main(['-m', 'process', '-s', '--alluredir', 'report/tmp'])  # -m运行mark标记文件
-    # os.system('allure generate report/tmp -o report/html --clean')  # /report/tmp 为存放报告的源文件目录
+    os.system('allure generate report/tmp -o report/html --clean')  # /report/tmp 为存放报告的源文件目录
