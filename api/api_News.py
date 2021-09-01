f91d71c9a5dbc3a0ce7131145d7df3e3d5e50887
weimeipeng <137434122@qq.com>
2021-09-01 17:22:41 +0800

    ss

--- api/api_News.py
+++ api/api_News.py
@@ -15,17 +15,29 @@ class Api_News_Service(object):
     def __init__(self):
         self.headers = HttpRequests().headers
 
-    def get_first_hot(self):
+    def get_hotnews(self):
         url = "http://v.juhe.cn/toutiao/index"
         param_data={'page':3,'key':'e4e13b9352c189d2289d26a97c8ef23c'}
         response = HttpRequests().get(url,headers=self.headers,params=param_data)
-        hot = response.json()['result']['data'][0]['title']
-        return hot
+        # hot = response.json()['result']['data'][0]['title']
+        return response
 
     def get_weather(self,city):
         url='http://apis.juhe.cn/simpleWeather/query'
         param_data = {'city':city,'key': 'fea71ec5629c802f1c7d64ba57bae351'}
         response = HttpRequests().get(url,params=param_data,headers=self.headers)
+        return response
+
+    def get_phone(self,phone):
+        url="http://apis.juhe.cn/mobile/get"
+        param_data = {'phone': phone,'dtype':"json" ,'key': '6e9a421a23a510891542fcd6a2c167d7'}
+        response = HttpRequests().get(url, params=param_data, headers=self.headers)
+        return response
+
+    def get_constellation(self,consName):
+        url = "http://web.juhe.cn:8080/constellation/getAll"
+        param_data = {'consName': consName, 'type': "today", 'key': 'f7aec031a4fcbccdafa7906300d365f8'}
+        response = HttpRequests().get(url, params=param_data, headers=self.headers)
         return response
 
 
